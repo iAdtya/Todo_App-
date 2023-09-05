3f9d367eac4658486ef95c2b11bd01b4c4edae54
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-09-06 02:27:21 +0530

    added the delete functionality

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,48 +1,53 @@
 const Todo = require("../models/user");
 
+//todo fetch data from database
 module.exports.tasks = async function (req, res) {
   try {
-  let todos = await Todo.find().select('-updatedAt -createdAt -__v').sort({_id:-1});
-  res.status(200).render("home", {
-    title: "Home",
-    todos:todos
-  });
-  }catch(err){
+    let todos = await Todo.find()
+      .select("-updatedAt -createdAt -__v")
+      .sort({ _id: -1 });
+    res.status(200).render("home", {
+      title: "Home",
+      todos: todos,
+    });
+  } catch (err) {
     console.log("Error in rendering home page", err);
     return res.status(500).send("Internal Server Error");
   }
-};    
+};
 
+//todo create new task
 module.exports.create = async function (req, res) {
   try {
     const newtodo = await Todo.create({
       description: req.body.description,
       category: req.body.category,
-      due_date: req.body.date
+      due_date: req.body.date,
     });
     console.log("New todo", newtodo);
     return res.redirect("back");
   } catch (err) {
     console.log("Error in creating a todo", err);
     return res.status(500).send("Internal Server Error"), res.redirect("back");
-    
   }
 };
 
+//todo delete task
 module.exports.delete = async function (req, res) {
+  try {
+    // Get the task IDs from query parameters
+    const taskIds = Object.keys(req.body);// Assuming the task IDs are passed as an array of IDs in the 'ids' parameter
+    console.log(taskIds);
 
-  // console.log(req.query);
-  const taskId = req.query.id;
-  const result = await Todo.findByIdAndDelete(taskId);
-    try{
-      if(result){
-        console.log("Todo deleted successfully");
-        return res.redirect("back");
-      }else{
-        return res.status(404).send("Not Found");
-      }
-    }catch(err){
-      console.log("Error in deleting a todo", err);
-      return res.status(500).send("Internal Server Error");
+    // Loop through each task ID and delete it
+    for (const taskId of taskIds) {
+        // Using async/await to handle asynchronous operations
+        await Todo.findByIdAndDelete(taskId);
     }
-} 
+
+    return res.redirect('back');
+} catch (error) {
+    console.error(error);
+    res.redirect('/');
+}
+};
